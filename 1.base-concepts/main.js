f4ae3d0931b4c1b747d55fc2216b21d3f546ef8a
Alexandr Alyabev <al-alexandr@mail.ru>
2022-10-04 09:28:53 +0300

    Homework done

--- 1.base-concepts/main.js
+++ 1.base-concepts/main.js
@@ -1,3 +1,4 @@
+"use strict"
 function calculateQuadraticEquation(){
     let a = +window.a.value;
     let b = +window.b.value;
@@ -15,7 +16,33 @@ function calculateMortgage() {
     let date = new Date(window.date.value);
 
     let result = calculateTotalMortgage(percent, contribution, amount, date);
+    //result = (result).toFixed(2);
     let span = window.mortageResult;
     span.textContent = result;
 }
 
+function calculateTotalMortgage(percent, contribution, amount, date) {
+  let totalAmount;
+  let credAmount = amount - contribution;
+  let period = date - new Date();
+  //console.log(period);
+  let denominator = 1000 * 60 * 60 * 24 * 30;
+  let month = Math.floor(period / denominator);
+    if(month <= 1) {
+    alert("минимальный период кредита - 1 мес. Укажите правильную дату");
+    return;
+  }
+    else {
+  percent = percent / 100;
+  let P = percent / 12;
+  let payment = credAmount * (P + (P / (((1 + P)**month) - 1)));
+  totalAmount = payment * month;
+  totalAmount = (totalAmount).toFixed(2);
+  console.log(totalAmount + typeof(totalAmount));
+    }
+
+
+  // код для задачи №2 писать здесь
+
+  return totalAmount;
+}
--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,11 +1,38 @@
-function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
+"use strict"
+function solveEquation(a,b,c) {
+  let discr = b**2-4*a*c;
+  let arr = new Array();
+  if (discr > 0) {
+    arr.push((-b + Math.sqrt(discr))/(2*a));
+    arr.push((-b - Math.sqrt(discr))/(2*a));
+  }
+  else if (discr == 0) {
+    arr.push(-b/(2*a));
+  }
+  return arr;
 }
+
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
+  let credAmount = amount - contribution;
+  let period = date - new Date();
+  //console.log(period);
+  let denominator = 1000 * 60 * 60 * 24 * 30;
+  let month = Math.floor(period / denominator);
+    if(month <= 1) {
+    alert("минимальный период кредита - 1 мес. Укажите правильную дату");
+    return;
+  }
+    else {
+  percent = percent / 100;
+  let P = percent / 12;
+  let payment = credAmount * (P + (P / (((1 + P)**month) - 1)));
+  totalAmount = payment * month;
+  totalAmount = (totalAmount).toFixed(2);
+  console.log(totalAmount + typeof(totalAmount));
+    }
+
 
   // код для задачи №2 писать здесь
 
