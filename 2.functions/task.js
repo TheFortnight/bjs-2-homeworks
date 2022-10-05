50be047d55f5e603f27b295997bc8e00563f0372
Alexandr Alyabev <al-alexandr@mail.ru>
2022-10-05 15:43:24 +0300

    Functions

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,7 +1,16 @@
 // Задание 1
 function getArrayParams(arr) {
   let min, max, sum, avg;
+  min = 100;
+  max = -100;
+  sum = 0;
 
+  for (let i=0; i<arr.length; i++){
+    min = min > arr[i]? arr[i] : min;
+    max = max < arr[i]? arr[i] : max;
+    sum += arr[i];
+  }
+  avg = +(sum / arr.length).toFixed(2);
   // Ваш код
 
   return { min: min, max: max, avg: avg };
@@ -9,16 +18,21 @@ function getArrayParams(arr) {
 
 // Задание 2
 function worker(arr) {
-  let sum;
-
+  let sum = 0;
+  for (let i=0; i<arr.length; i++) {
+    sum += arr[i];
+  }
   // Ваш код
 
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max;
-
+  let max = -Infinity;
+  for (let i=0; i<arrOfArr.length; i++) {
+    calcSum = func(arrOfArr[i]) 
+    max = max < calcSum? calcSum : max;
+  }
   // Ваш кода
   // for ...
   
@@ -28,4 +42,12 @@ function makeWork(arrOfArr, func) {
 // Задание 3
 function worker2(arr) {
   // Ваш код
+  let highest = -Infinity;
+  let lowest = +Infinity;
+  //let result;
+  for (let i=0; i<arr.length; i++) {
+    highest = highest < arr[i]? arr[i] : highest;
+    lowest = lowest > arr[i]? arr[i] : lowest;
+  }
+  return +(highest - lowest);
 }
