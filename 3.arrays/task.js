3fb13c34ff96c20182727a96b86bc8c908f8c667
Alexandr Alyabev <al-alexandr@mail.ru>
2022-10-05 19:11:43 +0300

    Arrays

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,6 +1,9 @@
 function compareArrays(arr1, arr2) {
   let result;
-
+  if(arr1.length !== arr2.length){
+    return result = false;
+  }
+    result = arr1.every((el,ind) => el === arr2[ind]);
   // Ваш код
 
   return result; // boolean
@@ -10,6 +13,8 @@ function advancedFilter(arr) {
   let resultArr;
 
   // Ваш код
-
+  resultArr = arr.filter((el) => el > 0)
+  .filter((elem) => elem % 3 === 0)
+  .map((e) => e * 10);
   return resultArr; // array
 }
