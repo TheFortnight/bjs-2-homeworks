76ffecee2b0ac7759914500fa135ad0f9987bfe8
Alexandr Alyabev <al-alexandr@mail.ru>
2022-10-10 19:23:18 +0300

    Objects

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,50 @@
 function Student(name, gender, age) {
     // Ваш код
-
+this.name = name;
+this.gender = gender;
+this.age = age;
 }
 
 Student.prototype.setSubject = function (subjectName) {
   //ваш код
+  this.subject = subjectName;
 }
 
 // ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+  if (this.marks === undefined) {
+    this.marks = [mark];
+  }
+  else {
+    this.marks.push(mark);
+  }
+}
+
+Student.prototype.addMarks = function (... marks) {
+  if (this.marks === undefined) {
+    this.marks = marks;
+  }
+  else {
+    this.marks = this.mark.concat(marks);
+  }
+}
+
+Student.prototype.getAverage = function () {
+  if (this.marks === undefined) {
+    console.log("no marks");
+    return;
+  }
+  else {
+    let count = 0;
+    for (m of this.marks){
+      count += m;
+    };
+    return this.average = count/this.marks.length;
+  }
+}
+
+Student.prototype.exclude = function (reason) {
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
+}
