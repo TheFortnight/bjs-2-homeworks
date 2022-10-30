d7b81944cd6abe2f88f70285816a2c89a000bda2
Alexandr Alyabev <al-alexandr@mail.ru>
2022-10-30 19:51:07 +0300

    Async

--- 7.async/task.js
+++ 7.async/task.js
@@ -11,8 +11,7 @@ class AlarmClock {
             console.error("this id already exists");
             return;
         } else {
-            this.alarmCollection.push(
-                {
+            this.alarmCollection.push({
                 id: actionId,
                 time: actionTime,
                 callback: actionFunc
@@ -37,41 +36,50 @@ class AlarmClock {
     getCurrentFormattedTime() {
         let date = new Date();
         let hours = date.getHours();
-        if(hours < 10){
-            hours = ("0"+hours);
+        if (hours < 10) {
+            hours = ("0" + hours);
         }
         let minutes = date.getMinutes();
-        if(minutes < 10){
-            minutes = ("0"+minutes);
+        if (minutes < 10) {
+            minutes = ("0" + minutes);
         }
         return `${hours}:${minutes}`
     }
     start() {
-        function checkClock(arg, scope) {
-            scope = typeof scope === null? this : scope;
-            let findTime = this.getCurrentFormattedTime.bind(scope);
-            let now = findTime();
+       
+       let getTime = this.getCurrentFormattedTime;
+        function parse() {
+            this.alarmCollection.forEach(al => checkClock(al));
+        }
+
+        function checkClock(arg) {
+            
+            let now = getTime();
             if (arg.time === now) {
                 arg.callback();
+            } else {
+                console.log("it's not time");
             }
         }
-        if(this.timerId === null){
+
+        if (this.timerId === null) {
             console.log("Id is null");
-            this.timerId = setInterval((scope) => {
-                this.alarmCollection.forEach(al => checkClock(al, scope));
-            }, 2000);
+            this.timerId = setInterval(() => parse.call(this), 1000);
+            //setTimeout(() => clearInterval(this.timerId), 5000);
         }
+
     }
-    stop(){
-        if(this.timerId !== "undefined"){
+    stop() {
+        if (this.timerId !== null) {
+            console.log("stop alarm");
             clearInterval(this.timerId);
             this.timerId = null;
         }
     }
-    printAlarms(){
+    printAlarms() {
         this.alarmCollection.forEach(el => console.log(`${el.id}, ${el.time}`))
     }
-    clearAlarms(){
+    clearAlarms() {
         this.stop();
         this.alarmCollection = [];
     }
@@ -79,13 +87,48 @@ class AlarmClock {
 }
 
 // Task 2
-function testCase(){
-let alarm = new AlarmClock();
-let curTime = alarm.getCurrentFormattedTime(alarm);
-function signal(){
-    console.log("alarm is on now");
+function testCase1() {
+    let alarm = new AlarmClock();
+    let curTime = alarm.getCurrentFormattedTime();
+
+    function signal() {
+        console.log("alarm is on now");
+    }
+    alarm.addClock(curTime, signal, 5000);
+    alarm.start();
+    setTimeout(() => clearInterval(alarm.timerId), 5000);
 }
-alarm.addClock(curTime, signal, 5000);
-alarm.start();
-setTimeout(() => clearInterval(alarm.timerId), 10000);
+
+function testCase2() {
+    let alarm = new AlarmClock();
+
+    function curTimePlusMinute() {
+        let date = new Date();
+        let hours = date.getHours();
+        if (hours < 10) {
+            hours = ("0" + hours);
+        }
+        let minutes = date.getMinutes() + 1;
+        if (minutes < 10) {
+            minutes = ("0" + minutes);
+        }
+        return `${hours}:${minutes}`
+    }
+    let delayedAlarm = curTimePlusMinute();
+    console.log("delayed time is " + delayedAlarm);
+
+    function signal() {
+        console.log("Delayed alarm is on now");
+    }
+    alarm.addClock(delayedAlarm, signal, 5000);
+    alarm.start();
+    alarm.printAlarms();
+    setTimeout(() => alarm.stop(), 60000);
+
 }
+
+testCase1();
+setTimeout(() => {
+    console.log("Running test2");
+    testCase2();
+}, 10000);
