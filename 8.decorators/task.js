172654bbaabca9a4ea16fea9d9cbf68e4adc4e3d
Alexandr Alyabev <al-alexandr@mail.ru>
2022-10-31 12:47:38 +0300

    Decorators

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,8 +1,42 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
+  const cache = [];
+  function wrapper(...args){
+    const hash = args.join(",");
+    let objectInCache = cache.find((item) => item.name === hash);
+    console.log("objectInCache: "+objectInCache);
+    if(typeof objectInCache != "undefined"){
+      console.log("returning cached result: "+objectInCache.result);
+      return "Из кэша: " + objectInCache.result;
+    }
+    let calcRes = func(...args);
+    if(cache.length === 5){
+      console.log("first position removed from cache");
+      cache.shift[0];
+    }
+    cache.push({"name": hash, "result": calcRes});
+    console.log("Вычисляем: " + calcRes);
+    return ("Вычисляем: " + calcRes);
+  }
+  return wrapper;
 }
 
 
-function debounceDecoratorNew(func) {
-  // Ваш код
+function debounceDecoratorNew(func, delay){
+  let timeoutId = null;
+  let count = 0;
+  let allCount = 0;
+  return function wrapper(...args){
+    if(timeoutId){
+      console.log("current timer cleared ", ++allCount);
+      clearTimeout(timeoutId);
+    }
+    console.log("creating new timeout");
+    timeoutId = setTimeout(() => {
+      console.log(func(...args),  ++count, ++allCount);
+      console.log("callback called");
+    }, delay);
+   return wrapper;
+  }
+  
 }
+
