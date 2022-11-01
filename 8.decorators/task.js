a87d7c138fd519b908e15afe7641120106ab3f57
Alexandr Alyabev <al-alexandr@mail.ru>
2022-11-01 14:56:54 +0300

    Decorators improved

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,19 +1,23 @@
 function cachingDecoratorNew(func) {
   const cache = [];
-  function wrapper(...args){
+
+  function wrapper(...args) {
     const hash = args.join(",");
     let objectInCache = cache.find((item) => item.name === hash);
-    console.log("objectInCache: "+objectInCache);
-    if(typeof objectInCache != "undefined"){
-      console.log("returning cached result: "+objectInCache.result);
+    console.log("objectInCache: " + objectInCache);
+    if (objectInCache) {
+      console.log("returning cached result: " + objectInCache.result);
       return "Из кэша: " + objectInCache.result;
     }
     let calcRes = func(...args);
-    if(cache.length === 5){
+    if (cache.length === 5) {
       console.log("first position removed from cache");
-      cache.shift[0];
+      cache.shift;
     }
-    cache.push({"name": hash, "result": calcRes});
+    cache.push({
+      "name": hash,
+      "result": calcRes
+    });
     console.log("Вычисляем: " + calcRes);
     return ("Вычисляем: " + calcRes);
   }
@@ -21,25 +25,31 @@ function cachingDecoratorNew(func) {
 }
 
 
-function debounceDecoratorNew(func, delay){
+function debounceDecoratorNew(func, delay) {
   let timeoutId = null;
-  let count = 0;
-  let allCount = 0;
-  return function wrapper(...args){
-    if(timeoutId){
-      console.log("current timer cleared ", ++allCount);
-      Function.prototype.allCount = allCount;
-      clearTimeout(timeoutId);
-    }
-    console.log("creating new timeout");
-    timeoutId = setTimeout(() => {
-      console.log(func(...args),  ++count, ++allCount);
-      console.log("callback called");
-    }, delay);
-    Function.prototype.count = count;
-    
-   return wrapper;
-  }
-  
-}
 
+  function wrapper(...args) {
+    ++wrapper.allCount;
+    if (!timeoutId) {
+      console.log(func(...args), ++wrapper.count);
+      console.log("callback called");
+      timeoutId = setTimeout(() => {
+        console.log(func(...args), ++wrapper.count);
+        console.log("delayed callback called");
+      }, delay);
+    } else if (timeoutId) {
+      console.log("current timer cleared");
+      clearTimeout(timeoutId);
+      console.log("creating new timeout");
+      timeoutId = setTimeout(() => {
+        console.log(func(...args), ++wrapper.count);
+        console.log("delayed callback called");
+      }, delay);
+    }
+
+  }
+  wrapper.count = 0;
+  wrapper.allCount = 0;
+  return wrapper;
+
+}
